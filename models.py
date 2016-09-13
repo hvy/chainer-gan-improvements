bf91eae8374b494899f4ba7c99ea394db80d0181
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2016-09-13 23:33:27 +0900

    Working minibatch discrimination

--- models.py
+++ models.py
@@ -18,36 +18,30 @@ def convdim(dims, scale, n):
 class MinibatchDiscrimination(Chain):
     def __init__(self, in_shape, n_kernels, kernel_dim):
         super(MinibatchDiscrimination, self).__init__(
-            fc0=L.Linear(in_shape, n_kernels * kernel_dim)
+            t=L.Linear(in_shape, n_kernels*kernel_dim)
         )
         self.n_kernels = n_kernels
         self.kernel_dim = kernel_dim
 
     def __call__(self, x):
-        h = self.fc0(x)
+        minibatch_size = x.shape[0]
 
-        print('h.shape', h.shape)
+        h = self.t(x)
 
         activation = F.reshape(h, (-1, self.n_kernels, self.kernel_dim))
-        diff_t1 = F.expand_dims(activation, axis=3)
-        diff_t2 = F.expand_dims(F.transpose(activation, (1, 2, 0)), axis=0)
-        diff_t1, diff_t2 = F.broadcast(diff_t1, diff_t2)
-        diff = diff_t1 - diff_t2
+        activation_ex = F.expand_dims(activation, 3)
+        activation_ex_t = F.expand_dims(F.transpose(activation, (1, 2, 0)), 0)
+        activation_ex, activation_ex_t = F.broadcast(activation_ex, activation_ex_t)
+        diff = activation_ex - activation_ex_t
 
-        print('diff.shape', diff.shape)
-
-        xp = chainer.cuda.get_array_module()
-        eps = F.expand_dims(xp.eye(x.data.shape[0], dtype=xp.float32), 1)
+        xp = chainer.cuda.get_array_module(x.data)
+        eps = F.expand_dims(xp.eye(minibatch_size, dtype=xp.float32), 1)
+        eps = F.broadcast_to(eps, (minibatch_size, self.n_kernels, minibatch_size))
         sum_diff = F.sum(abs(diff), axis=2)
-        sum_diff, eps = F.broadcast(sum_diff, eps)
+        sum_diff = F.broadcast_to(sum_diff, eps.shape)
         abs_diff = sum_diff + eps
 
-        print('abs_diff.shape', abs_diff.shape)
-
         minibatch_features = F.sum(F.exp(-abs_diff), 2)
-
-        print('x.shape', x.shape)
-        print('minibatch_features.shape', minibatch_features.shape)
 
         return F.concat((x, minibatch_features), axis=1)
 
@@ -68,7 +62,8 @@ class Generator(Chain):
         self.out_shape = out_shape
 
     def __call__(self, z, test=False):
-        h = F.reshape(F.relu(self.bn0(self.fc0(z), test=test)), ((z.data.shape[0],) + convdim(self.out_shape, 2**4, 256)))
+        h = F.relu(self.bn0(self.fc0(z), test=test))
+        h = F.reshape(h, ((z.shape[0],) + convdim(self.out_shape, 2**4, 256)))
         h = F.relu(self.bn1(self.dc1(h), test=test))
         h = F.relu(self.bn2(self.dc2(h), test=test))
         h = F.relu(self.bn3(self.dc3(h), test=test))
@@ -83,18 +78,27 @@ class Discriminator(Chain):
             c1=L.Convolution2D(32, 64, 4, stride=2, pad=1),
             c2=L.Convolution2D(64, 128, 4, stride=2, pad=1),
             c3=L.Convolution2D(128, 256, 4, stride=2, pad=1),
-            fc4=L.Linear(lindim(in_shape, 2**4, 256), 2),
+            fc4=L.Linear(lindim(in_shape, 2**4, 256), 512),
+            mbd=MinibatchDiscrimination(512, 32, 8),
+            fc5=L.Linear(512, 512+32),  # Alternative to minibatch discrimination
+            fc6=L.Linear(512+32, 2),
             bn1=L.BatchNormalization(64),
             bn2=L.BatchNormalization(128),
-            bn3=L.BatchNormalization(256),
-            mbd=MinibatchDiscrimination(1024, 32, 8)
+            bn3=L.BatchNormalization(256)
         )
 
-    def __call__(self, x, test=False):
+    def __call__(self, x, minibatch_discrimination=True, test=False):
         h = F.leaky_relu(self.c0(x))
         h = F.leaky_relu(self.bn1(self.c1(h), test=test))
         h = F.leaky_relu(self.bn2(self.c2(h), test=test))
         h = F.leaky_relu(self.bn3(self.c3(h), test=test))
-        h = self.mbd(h)
         h = self.fc4(h)
+
+        if minibatch_discrimination:
+            h = self.mbd(h)
+        else:
+            h = F.leaky_relu(self.fc5(h))
+
+        h = self.fc6(h)
+
         return h
--- train.py
+++ train.py
@@ -1,26 +1,29 @@
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
 import argparse
 import numpy as np
 from PIL import Image
-from chainer import Variable, datasets, cuda
+from chainer import datasets, cuda, serializers
 from chainer import optimizers as O
 from chainer import functions as F
 from models import Generator, Discriminator
+
+
+# Resize the MNIST dataset to 32x32 images for convenience
+# since the generator will create images with dimensions
+# of powers of 2 (doubling upsampling in each deconvolution).
+im_shape = (32, 32)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpu', type=int, default=-1)
     parser.add_argument('--n-z', type=int, default=10)
-    parser.add_argument('--max-epochs', type=int, default=1000)
+    parser.add_argument('--max-epochs', type=int, default=100)
     parser.add_argument('--batch-size', type=int, default=128)
+    parser.add_argument('--out-generator-filename', type=str, default='./generator.model')
     return parser.parse_args()
 
 
-def resize(im, shape):
+def resize_im(im, shape):
     im = Image.fromarray(im)
     im = im.resize(shape, Image.ANTIALIAS)
     return np.asarray(im)
@@ -32,24 +35,21 @@ if __name__ == '__main__':
     n_z = args.n_z
     max_epochs = args.max_epochs
     batch_size = args.batch_size
+    out_generator_filename = args.out_generator_filename
 
-    if gpu >= 0:
-        xp = cuda.cupy
-    else:
-        xp = np
-
+    # Prepare the training data
     train, _ = datasets.get_mnist(withlabel=False, ndim=2)
-    train = [resize(t, (32, 32)) for t in train]
-    train = xp.asarray(train)
-
+    train = [resize_im(t, im_shape) for t in train]
+    train = np.asarray(train)
     train_size = train.shape[0]
-    in_shape = train.shape[1:]
+    assert train.shape[1:] == im_shape
 
-    generator = Generator(n_z, in_shape)
+    # Prepare the models
+    generator = Generator(n_z, im_shape)
     generator_optimizer = O.Adam(alpha=1e-3, beta1=0.5)
     generator_optimizer.setup(generator)
 
-    discriminator = Discriminator(in_shape)
+    discriminator = Discriminator(im_shape)
     discriminator_optimizer = O.Adam(alpha=2e-4, beta1=0.5)
     discriminator_optimizer.setup(discriminator)
 
@@ -58,30 +58,29 @@ if __name__ == '__main__':
         cuda.get_device(gpu).use()
         generator.to_gpu()
         discriminator.to_gpu()
+        xp = cuda.cupy
+    else:
+        xp = np
 
     for epoch in range(max_epochs):
-
         generator_epoch_loss = np.float32(0)
         discriminator_epoch_loss = np.float32(0)
 
         for i in range(0, train_size, batch_size):
             # Forward
             zs = xp.random.uniform(-1, 1, (batch_size, n_z)).astype(xp.float32)
-            x_fake = generator(Variable(zs))
-            print('x_fake.shape', x_fake.shape)
+            x_fake = generator(zs)
             y_fake = discriminator(x_fake)
-            print('y_fake.shape', y_fake.shape)
-            x_real = xp.zeros((batch_size, *in_shape), dtype=xp.float32)
+            x_real = xp.zeros((batch_size, *im_shape), dtype=xp.float32)
             for xi in range(len(x_real)):
-                x_real[xi] = train[xp.random.randint(train_size)]
+                x_real[xi] = xp.array(train[np.random.randint(train_size)])
             x_real = xp.expand_dims(x_real, 1)  # Grayscale channel axis
-            y_real = discriminator(Variable(x_real))
-            print('y_real.shape', y_real.shape)
+            y_real = discriminator(x_real)
 
             # Losses
-            generator_loss = F.softmax_cross_entropy(y_fake, Variable(xp.ones(batch_size, dtype=xp.int32)))
-            discriminator_loss = F.softmax_cross_entropy(y_fake, Variable(xp.zeros(batch_size, dtype=xp.int32)))
-            discriminator_loss += F.softmax_cross_entropy(y_real, Variable(xp.ones(batch_size, dtype=xp.int32)))
+            generator_loss = F.softmax_cross_entropy(y_fake, xp.ones(batch_size, dtype=xp.int32))
+            discriminator_loss = F.softmax_cross_entropy(y_fake, xp.zeros(batch_size, dtype=xp.int32))
+            discriminator_loss += F.softmax_cross_entropy(y_real, xp.ones(batch_size, dtype=xp.int32))
             discriminator_loss /= 2
 
             # Backprop
@@ -96,10 +95,13 @@ if __name__ == '__main__':
             generator_epoch_loss += generator_loss.data
             discriminator_epoch_loss += discriminator_loss.data
 
-        generator_avg_loss = round(generator_epoch_loss / train_size, 10)
-        discriminator_avg_loss = round(discriminator_epoch_loss / train_size, 10)
+        generator_avg_loss = generator_epoch_loss / train_size
+        discriminator_avg_loss = discriminator_epoch_loss / train_size
 
         print('Epoch {} Loss Generator: {} Loss Discriminator: {}'
-            .format(epoch + 1, generator_avg_loss, discriminator_avg_loss))
+              .format(epoch + 1, generator_avg_loss, discriminator_avg_loss))
+
+    print('Saving model', out_generator_filename)
+    serializers.save_hdf5(out_generator_filename, generator)
 
     print('Finished training')
